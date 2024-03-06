fd2b282d4c906bed80d68383a82faa631c3c1a06
itsagrawal <162340533+itsagrawal@users.noreply.github.com>
2024-03-06 09:00:34 +0530

    Update firebase.js

--- AgroSite-react/src/firebase/firebase.js
+++ AgroSite-react/src/firebase/firebase.js
@@ -8,12 +8,6 @@ import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCEW_Eck7ra6uT0AvZtaRUfg6oRWZ4YMkI",
-  authDomain: "agrosite-2.firebaseapp.com",
-  projectId: "agrosite-2",
-  storageBucket: "agrosite-2.appspot.com",
-  messagingSenderId: "834895861105",
-  appId: "1:834895861105:web:70decd8bde0ce64a1b92d0",
 };
 
 // Initialize Firebase
